92d6b9f00a42f5d5e00a45342f5a7a0354bae20a
Udvas <uacharjee14@gmail.com>
2022-02-19 11:38:11 +0530

    Added reciprocal and divide functionalities

--- Complex_Class.py
+++ Complex_Class.py
@@ -5,7 +5,7 @@
 
 class Complex:
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
     def __str__(self):
@@ -13,13 +13,25 @@ class Complex:
     #we shall define the operations now
 
     def add(this, complex2):
-    #Add here and return object
+        return Complex(this.x+complex2.x, this.y+complex2.y)
+
+        
+   
+
 
     def subtract(this, complex2):
         #Subtract here and return object
+        return Complex(this.x-complex2.x, this.y-complex2.y)
     def multiply(this, complex2):
+        return Complex(this.x*complex2.x-this.y*complex2.y, this.x*complex2.y+this.y*complex2.x)
         #Multiply here and return object
-    def reciprocal(this, complex2):
+    def reciprocal(this):
+        if(this.x == 0 and this.y == 0):
+            raise ZeroDivisionError("Attempt to divide by zero")
+        r = this.x**2+this.y**2
+        return Complex(this.x/r, -this.y/r)
+        
         #reciprocal here and return object
     def divide(this, complex2):
+        return this.multiply(complex2.reciprocal());
         #divide here and return object
