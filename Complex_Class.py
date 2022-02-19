e45ea6216578ca0ef8427abd46fc8f2ab8a90fb6
Udvas <uacharjee14@gmail.com>
2022-02-19 11:11:03 +0530

    adding gui example and comple class

---
+++ Complex_Class.py
@@ -0,0 +1,25 @@
+#Program to implement basic complex number arithmetic
+
+#the arithmetic operations implemented:
+#addition, substaction, multiplication, division, reciprocal
+
+class Complex:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'{self.x}+i{self.y}'
+    #we shall define the operations now
+
+    def add(this, complex2):
+    #Add here and return object
+
+    def subtract(this, complex2):
+        #Subtract here and return object
+    def multiply(this, complex2):
+        #Multiply here and return object
+    def reciprocal(this, complex2):
+        #reciprocal here and return object
+    def divide(this, complex2):
+        #divide here and return object
